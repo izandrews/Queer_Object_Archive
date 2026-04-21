d684e109bdf7bd9aa70ca8ef1b0f3de803354be2
izandrews <isabelandrews101@gmail.com>
2026-04-21 11:23:12 +0100

    github workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 // @ts-check
 
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
 
-export default defineConfig({
-  integrations: [react()],
-});
+export default {
+  output: 'static'
+}
