50a92b299bf436ec83f1c123e0d80c3e743a53be
izandrews <isabelandrews101@gmail.com>
2026-04-22 12:46:09 +0100

    change base for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 
-//export default defineConfig({
-//  site: 'https://queerobjectarchive.com',
-//  base: '/',
-//});
-
-
 export default defineConfig({
-  site: 'https://izandrews.github.io',
-  base: '/Queer_Object_Archive/',
+  site: 'https://queerobjectarchive.com',
+  base: '/',
 });
+
+
+//export default defineConfig({
+//  site: 'https://izandrews.github.io',
+//  base: '/Queer_Object_Archive/',
+//});
